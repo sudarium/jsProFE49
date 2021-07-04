7550f12ef820a7597e78bb2c4abe21dfe84f2883
e.antonov <zhekasudarium@gmail.com>
2021-07-04 20:02:11 +0300

    add 2 solution for task

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+// 1st solution  
+
+// let defaultArray = [ [ "foo", "foo@foo.com" ],[ "bar_", "bar@bar.com" ], ["poo_", "poo@poo.com"] ];
+
+// let newArray = [];
+
+// for (let i = 0; i < defaultArray.length; i++){
+//     if(/_/.test(defaultArray[i][0])){
+//         newArray.push(defaultArray[i])
+//     }
+// }
+// console.log(newArray);
+
+
+
+// 2nd solution
+
 let defaultArray = [ [ "foo", "foo@foo.com" ],[ "bar_", "bar@bar.com" ], ["poo_", "poo@poo.com"] ];
 
-let fixArray = [];
-
-for (let i = 0; i < defaultArray.length; i++){
-    if(/_/.test(defaultArray[i][0])){
-        fixArray.push(defaultArray[i])
-    }
-}
-console.log(fixArray);
+let newArray = defaultArray.filter(user =>/_/.test(user)); 
+console.log(newArray);
