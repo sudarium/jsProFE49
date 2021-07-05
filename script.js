9e8eeabfd93b167d5429fca2e457a5939afe81d0
e.antonov <zhekasudarium@gmail.com>
2021-07-05 22:32:23 +0300

    edit solution

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+// function deleteSmallestIndex(inputArray) {
+
+//     let comparison = +Infinity;
+//     let arrayIndexCount = 0;
+
+//     if (inputArray.length == 0) {
+//         return inputArray;
+//     } else {
+//         for (let i = 0, j = 0; i < inputArray.length; i++, j++) {
+//             if (inputArray[i] < comparison) {
+//                 comparison = inputArray[i];
+//                 arrayIndexCount = j;
+//             } else {
+//                 comparison = comparison;
+//             }
+//         }
+//         inputArray.splice(arrayIndexCount, 1);
+//     }
+//     return inputArray
+// }
+// console.log(deleteSmallestIndex([11, 2, 3, 1, 8, 1]));
+// console.log(deleteSmallestIndex([]));
+
 function deleteSmallestIndex(inputArray) {
 
-    let comparison = +Infinity;
+    let comparison = inputArray[0];
     let arrayIndexCount = 0;
 
     if (inputArray.length == 0) {
@@ -10,9 +33,7 @@ function deleteSmallestIndex(inputArray) {
             if (inputArray[i] < comparison) {
                 comparison = inputArray[i];
                 arrayIndexCount = j;
-            } else {
-                comparison = comparison;
-            }
+            } 
         }
         inputArray.splice(arrayIndexCount, 1);
     }
