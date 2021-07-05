5fef64a22d02b5782f84ec4fce7dbb46d022d4a4
e.antonov <zhekasudarium@gmail.com>
2021-07-05 21:57:40 +0300

    edit solution for task

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function dontGiveMeFive(firsNumber, lastNumber) {
-
     let result = 0;
 
     for (let i = firsNumber; i < lastNumber + 1; i++) {
-        
-        if (String(i).includes('5')) {
-            result = result;
-        } else {
+
+        if (!(String(i).includes('5'))) {
             result += 1;
         }
     }
     return result;
 }
-console.log(dontGiveMeFive(25, 95));
+console.log(dontGiveMeFive(1, 5));
+
+
